1e6e97f06ff2a4e1c4218d543b1882fb015f2f52
dev-jgk <devkkwak@gmail.com>
2023-03-22 00:11:40 +0900

    test 4(https://school.programmers.co.kr/learn/courses/30/lessons/120883)

--- src/main/java/org/example/test4.java
+++ src/main/java/org/example/test4.java
@@ -6,7 +6,9 @@ public class test4 {
     public static void main(String[] args) {
 
         String[] id_pw = {"meosseugi", "1234"};
+
         String[][] db = {{"rardss", "123"}, {"yyoom", "1234"}, {"meosseugi", "1234"}};
+
         System.out.println(solution(id_pw, db));
     }
     public static String solution(String[] id_pw, String[][] db) {
